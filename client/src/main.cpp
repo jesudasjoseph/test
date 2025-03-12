1aeaea407664299ad03d5599e3f22d08d2adb008
Jesudas <jesudasmjoseph@gmail.com>
2025-03-11 20:27:19 -0700

    Add EBO

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -26,21 +26,13 @@ int main() {
 
   glViewport(0, 0, 800, 600);
 
-  GLfloat opacity = 0.0f;
-
-  float vertices[] = {
-      -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f,
-  };
-
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
-
   /*
   Vertex Shader Declaration + Compilation, no checks
   */
   const char* vertexShaderSource =
       "#version 330 core\n"
       "layout (location = 0) in vec3 aPos;\n"
+      "void main()\n"
       "{\n"
       "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
       "}\0";
@@ -56,11 +48,11 @@ int main() {
   */
   const char* fragmentShaderSource =
       "#version 330 core\n"
-      "out vec4 FragColor;\n\n"
+      "out vec4 FragColor;\n"
       "void main()\n"
       "{\n"
-      "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-      "}\0";
+      "FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
+      "}\n\0";
 
   unsigned int fragmentShader;
   fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -79,16 +71,31 @@ int main() {
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  /*
-  VAO
-  */
-  unsigned int VAO;
+  float vertices[] = {
+      0.5f,  0.5f,  0.0f, 0.5f,  -0.5f, 0.0f,
+      -0.5f, -0.5f, 0.0f, -0.5f, 0.5f,  0.0f,
+  };
+  unsigned int indices[] = {0, 1, 3, 1, 2, 3};
+
+  unsigned int VBO, VAO, EBO;
+  glGenBuffers(1, &VBO);
   glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &EBO);
+
   glBindVertexArray(VAO);
+
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 
   while (!glfwWindowShouldClose(window)) {
     // Simple Key detection
@@ -96,21 +103,28 @@ int main() {
       glfwSetWindowShouldClose(window, true);
     }
 
-    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
-      opacity += 0.01f;
-    }
-
-    glClearColor(0.2f, 0.5f, opacity, 1.0f);
+    glClearColor(0.2f, 0.5f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
+  // Cleanup
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+  glDeleteProgram(shaderProgram);
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // -----------------------------------------------------------------
   glfwTerminate();
   return 0;
 }
